0b81c704ea2a6c7af7fe146c005a9239645bfdc8
hieudominh <hieudm.qptb@gmail.com>
2023-07-29 22:43:27 +0700

    adding sum function

--- main.cpp
+++ main.cpp
@@ -1,8 +1,13 @@
 #include<iostream>
 
+int sum(int a, int b)
+{
+	return a + b;
+}
 
 int main()
 {
 	std::cout << "Hello World" << std::endl;
+	std::cout << "Sum: " << sum(10, 12) << std::endl;
 	return 0;
 }
